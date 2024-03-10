c927c38b72453b0b1aacb5283f03a63b1e57387a
Cleber Margarida <c.margarida@kaizengaming.com>
2024-03-10 13:33:17 -0300

    feat(api): configure mssql integration with api #11

--- src/SmartFleets.Api/Program.cs
+++ src/SmartFleets.Api/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using SmartFleets.Api;
 using SmartFleets.Application.Handlers;
+using SmartFleets.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("smartfleets.db"));
+});
 builder.AddServiceDefaults();
 
 builder.Services.AddRabbitMq();
